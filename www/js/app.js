5d8c0a0b8b0cbb40e87b280519853fe19cf7d54e
Ray Villalobos <ray@lynda.com>
2015-03-19 21:49:18 -0400

    Working with Angular Controllers

--- www/js/app.js
+++ www/js/app.js
@@ -17,3 +17,9 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.controller('ListController', ['$scope', '$http', function($scope, $http) {
+    $http.get('js/data.json').success(function(data) {
+      $scope.artists = data;  
+    });
+}]);
