8a99fa27840a3504681241f33231e6778e45136f
Ray Villalobos <ray@lynda.com>
2015-03-21 22:27:12 -0400

    03_05 delete functionality

--- www/js/app.js
+++ www/js/app.js
@@ -21,6 +21,11 @@ angular.module('starter', ['ionic'])
 .controller('ListController', ['$scope', '$http', function($scope, $http) {
     $http.get('js/data.json').success(function(data) {
       $scope.artists = data;  
+
+      $scope.onItemDelete = function(item) {
+        $scope.artists.splice($scope.artists.indexOf(item), 1);
+      }
+
       $scope.moveItem = function(item, fromIndex, toIndex) {
         $scope.artists.splice(fromIndex, 1);
         $scope.artists.splice(toIndex, 0, item);
