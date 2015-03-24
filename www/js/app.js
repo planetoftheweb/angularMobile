9ceb2e2ead1b74a5c93eec346d9bee7476f39645
Ray Villalobos <ray@lynda.com>
2015-03-23 22:17:19 -0400

    04_05 Creating Global Headers

--- www/js/app.js
+++ www/js/app.js
@@ -66,6 +66,7 @@ angular.module('starter', ['ionic'])
     $http.get('js/data.json').success(function(data) {
       $scope.artists = data;
       $scope.whichartist=$state.params.aId;
+      $scope.data = { showDelete: false, showReorder: false };
 
       $scope.onItemDelete = function(item) {
         $scope.artists.splice($scope.artists.indexOf(item), 1);
