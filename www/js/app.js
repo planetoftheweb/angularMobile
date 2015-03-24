6452a10b1b686b3bc9f3696239223373a70de39f
Ray Villalobos <ray@lynda.com>
2015-03-23 22:56:09 -0400

    04_06 Creating a Calendar Controller

--- www/js/app.js
+++ www/js/app.js
@@ -56,15 +56,47 @@ angular.module('starter', ['ionic'])
       }
     })
 
+    .state('tabs.calendar', {
+      url: '/calendar',
+      views: {
+        'calendar-tab' : {
+          templateUrl: 'templates/calendar.html',
+          controller: 'CalendarController'
+        }
+      }
+    })
+
 
   $urlRouterProvider.otherwise('/tab/home');
 })
 
+.controller('CalendarController', ['$scope', '$http', '$state',
+    function($scope, $http, $state) {
+    $http.get('js/data.json').success(function(data) {
+      $scope.calendar = data.calendar;
+
+      $scope.onItemDelete = function(dayIndex, item) {
+        $scope.calendar[dayIndex].schedule.splice($scope.calendar[dayIndex]().schedule.indexOf(item), 1);
+      }
+
+      $scope.doRefresh =function() {
+      $http.get('js/data.json').success(function(data) {
+          $scope.calendar = data.calendar;
+          $scope.$broadcast('scroll.refreshComplete');
+        });
+      }
+
+      $scope.toggleStar = function(item) {
+        item.star = !item.star;
+      }
+
+    });
+}])
 
 .controller('ListController', ['$scope', '$http', '$state',
     function($scope, $http, $state) {
     $http.get('js/data.json').success(function(data) {
-      $scope.artists = data;
+      $scope.artists = data.artists;
       $scope.whichartist=$state.params.aId;
       $scope.data = { showDelete: false, showReorder: false };
 
