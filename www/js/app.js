30c20b6b2b346d81a41ca8f7bf0738bf60f3c9ee
vikbert <segentor@gmail.com>
2016-01-04 16:15:58 +0100

    fixed the problem if doRefresh

--- www/js/app.js
+++ www/js/app.js
@@ -106,7 +106,7 @@ angular.module('starter', ['ionic'])
 
       $scope.doRefresh =function() {
       $http.get('js/data.json').success(function(data) {
-          $scope.artists = data;
+          $scope.artists = data.artists;
           $scope.$broadcast('scroll.refreshComplete'); 
         });
       }
